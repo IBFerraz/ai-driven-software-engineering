d1b47158acd8b9f04d2d830620b00d0810e98eff
Isaac Ferraz <ifezol7@gmail.com>
2026-01-14 22:01:21 -0300

    Adiciona configuração de CI com GitHub Actions

--- tests/orderService.test.js
+++ tests/orderService.test.js
@@ -4,6 +4,10 @@ test('deve aplicar desconto quando valor for maior que 100', () => {
   expect(calculateOrderTotal(200)).toBe(180);
 });
 
-test('não deve aplicar desconto quando valor for menor ou igual a 100', () => {
+test('deve manter valor igual a 100 sem desconto', () => {
   expect(calculateOrderTotal(100)).toBe(100);
+});
+
+test('não deve aplicar desconto quando valor for menor que 100', () => {
+  expect(calculateOrderTotal(50)).toBe(50);
 });
